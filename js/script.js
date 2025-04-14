e6822ce82416d6ce24f936917dd79902a2b9b173
leonardoSerilli <leoser14592@gimail.com>
2025-04-14 15:39:32 +0200

    TSK-70: header

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function asd() {
+  if (document.getElementById("asd").value == "") {
+    alert("Please enter a value");
+  } else {
+    alert("Value entered: " + document.getElementById("asd").value);
+  }
+}
