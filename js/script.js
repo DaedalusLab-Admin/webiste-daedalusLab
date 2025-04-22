921b3897435b276ca1d7e4d0818c79a5af024200
leonardoSerilli <leoser14592@gimail.com>
2025-04-22 17:54:43 +0200

    TSK-70: responsivness

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ function changeLanguage(language) {
   console.log("Language changed to:", language);
   // Add your language-switching logic here
 }
+
+function toggleBurgerMenu() {
+  const nav = document.getElementById("navigationNav");
+  nav.classList.toggle("active");
+}
